cf654e947afdfdc39547f54c37cdfc3532b1f1a3
Eric Abib <ehheric@gmail.com>
2023-04-22 16:48:32 -0300

    Tela de cadastro

--- app/src/main/java/br/edu/puc/toothnow/CreateActivity.kt
+++ app/src/main/java/br/edu/puc/toothnow/CreateActivity.kt
@@ -3,16 +3,23 @@ package br.edu.puc.toothnow
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class CreateActivity : AppCompatActivity() {
 
     lateinit var btnCadastrar: AppCompatButton
+    lateinit var etEmail : AppCompatEditText
+    lateinit var etSenha : AppCompatEditText
+    lateinit var etConfirmarSenha: AppCompatEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
         btnCadastrar = findViewById(R.id.btnCadastrar)
+        etEmail = findViewById(R.id.etemail)
+        etSenha = findViewById(R.id.etSenha)
+        etConfirmarSenha = findViewById(R.id.etConfirmarSenha)
 
     }
 }
