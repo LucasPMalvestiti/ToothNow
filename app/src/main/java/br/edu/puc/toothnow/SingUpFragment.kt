c6b8de80ddda81b4b503200ad81af1cc630a8923
Eric Abib <ehheric@gmail.com>
2023-04-25 22:01:31 -0300

    Tela de cadastro 50%

--- app/src/main/java/br/edu/puc/toothnow/SingUpFragment.kt
+++ app/src/main/java/br/edu/puc/toothnow/SingUpFragment.kt
@@ -8,10 +8,13 @@ import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.functions.FirebaseFunctions
+import com.google.gson.GsonBuilder
 
 class SingUpFragment : Fragment() {
 
     private val TAG = "SingUpFragment"
     private lateinit var auth: FirebaseAuth
     private lateinit var functions: FirebaseFunctions
+
+    private val gson = GsonBuilder().enableComplexMapKeySerialization().create()
 }
